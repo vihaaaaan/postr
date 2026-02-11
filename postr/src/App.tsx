cf07075f543ab8f545256445516bf17d86b5c4ec
vihaaaaan <vihaanv14@gmail.com>
2026-02-11 01:16:28 -0500

    removed welcome modal

--- postr/src/App.tsx
+++ postr/src/App.tsx
@@ -1,21 +1,14 @@
 import SearchBar from "./components/SearchBar"
 import Sidebar from "./components/Sidebar";
-import WelcomeModal from "./components/WelcomeModal";
-
 
 function App() {
-    
     return (
-        <>
-            <WelcomeModal />
-            <div className="min-h-screen bg-gray-50 flex">
-                <Sidebar/>
-                <div className="flex-1 overflow-auto">
-                    <SearchBar/>
-                </div>
+        <div className="min-h-screen bg-gray-50 flex">
+            <Sidebar/>
+            <div className="flex-1 overflow-auto">
+                <SearchBar/>
             </div>
-
-        </>
+        </div>
     );
 }
 
